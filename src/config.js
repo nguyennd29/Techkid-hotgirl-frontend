616b245152e6dd77b8fec17820315c59bc237fd9
nguyennd29 <duynguyen29898@gmail.com>
2018-10-09 02:10:51 +0700

    update server url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export default {
     rootPath: 
     process.env.REACT_APP_STATE === "localhost"
     ? "http://localhost:6969"
-    : 'https://techkid-hotgirl-backend.herokuapp.com/'
+    : 'https://techkid-hotgirl-backend.herokuapp.com'
 };
 
